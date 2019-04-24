982277c762c7de275a094788318f87a74275504a
Clement <clementjoe99@gmail.com>
2019-04-24 19:51:01 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from telegram.ext import Updater, CommandHandler
+import requests
+import re
+
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()
+    url = contents['url']
+    return url
+
+def bop(bot, update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+def main():
+    updater = Updater('740875622:AAEr1ekgn8CUhLy9IS-BruKoYkUSH-mP9fc')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop',bop))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
